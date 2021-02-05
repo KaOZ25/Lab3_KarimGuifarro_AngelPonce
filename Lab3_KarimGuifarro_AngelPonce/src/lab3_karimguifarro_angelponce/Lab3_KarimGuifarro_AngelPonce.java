92f8a4430cd0f6bab48f79ea661a3307bfa9b1c2
KaOz25 <karimozael@gmail.com>
2021-02-05 17:33:04 -0600

    tercerp

--- Lab3_KarimGuifarro_AngelPonce/src/lab3_karimguifarro_angelponce/Lab3_KarimGuifarro_AngelPonce.java
+++ Lab3_KarimGuifarro_AngelPonce/src/lab3_karimguifarro_angelponce/Lab3_KarimGuifarro_AngelPonce.java
@@ -9,7 +9,8 @@ public class Lab3_KarimGuifarro_AngelPonce {
     public static ArrayList<Corporaciones> corporaciones = new ArrayList();
     public static ArrayList<Marcas> marcas = new ArrayList();
     public static ArrayList<Fabricas> fabricas = new ArrayList();
-     public static ArrayList<Divisiones_Tech> tech = new ArrayList();
+    public static ArrayList<Divisiones_Tech> tech = new ArrayList();
+    public static ArrayList<Modelos> modelos = new ArrayList();
 
     public static void main(String[] args) {
         char k = 'c';
@@ -50,11 +51,11 @@ public class Lab3_KarimGuifarro_AngelPonce {
                         }
                         break;
                         case 2: {
-                            
+
                         }
                         break;
                         case 3: {
-                            
+
                         }
                         break;
                         case 4: {
@@ -85,7 +86,31 @@ public class Lab3_KarimGuifarro_AngelPonce {
                     int p = KaOz.nextInt();
                     switch (p) {
                         case 1: {
-
+                            System.out.println("Agregando marcas...");
+                            for (int i = 0; i < corp.size(); i++) {
+                                System.out.println("Posicion " + i + ": " + corp.get(i).getNombre());
+                            }
+                            System.out.println("Ingrese la posicion de la corporacion: ");
+                            int posicion = KaOz.nextInt();
+                            System.out.println(corp.get(p).getNombre());
+                            String nombre, slogan, fundador, presidente;
+                            int yearfundacion, yearIntegro, numModelos; //número de modelos: size del arraylist Modelos;
+                            double totalVentas;
+                            System.out.println("Ingrese nombre de la marca: ");
+                            nombre = KaOz.next();
+                            System.out.println("Ingrese slogan: ");
+                            slogan = KaOz.next();
+                            System.out.println("Fundador: ");
+                            fundador = KaOz.next();
+                            System.out.println("Nombre del presidente de marca: ");
+                            presidente = KaOz.next();
+                            System.out.println("Anio de fundacion de la marca: ");
+                            yearfundacion = KaOz.nextInt();
+                            System.out.println("Anio de integracion: ");
+                            yearIntegro = KaOz.nextInt();
+                            System.out.println("Total de ventas: ");
+                            totalVentas = KaOz.nextDouble();
+                            marcas.add(new Marcas(nombre, slogan, fundador, presidente, yearfundacion, yearIntegro, numModelos, totalVentas));
                         }
                         break;
                         case 2: {
@@ -147,14 +172,21 @@ public class Lab3_KarimGuifarro_AngelPonce {
                     int p = KaOz.nextInt();
                     switch (p) {
                         case 1: {
-                            String nombre =KaOz.next();
-                            String ubicacion=KaOz.next();
-                            int cant_empleados=KaOz.nextInt();
-                            int cantmax_modelos=KaOz.nextInt();
-                            String tipo_carroceria=KaOz.next();
-                            String nombre_marcas=KaOz.next();
-                            int cant_autos=KaOz.nextInt();
-                            fabricas.add(new Fabricas(nombre, ubicacion,tipo_carroceria,nombre_marcas,cantmax_modelos,cant_empleados,cant_autos));
+                            System.out.println("Ingrese el nombre");
+                            String nombre = KaOz.next();
+                            System.out.println("Ingrese la ubicacion");
+                            String ubicacion = KaOz.next();
+                            System.out.println("Ingrese la cantidad de empleados");
+                            int cant_empleados = KaOz.nextInt();
+                            System.out.println("Ingrese la cantidad maxima de modelos");
+                            int cantmax_modelos = KaOz.nextInt();
+                            System.out.println("Ingrese el tipo de carroceria");
+                            String tipo_carroceria = KaOz.next();
+                            System.out.println("Ingrese el nombre de las marcas");
+                            String nombre_marcas = KaOz.next();
+                            System.out.println("Ingrese la cantidad de autos");
+                            int cant_autos = KaOz.nextInt();
+                            fabricas.add(new Fabricas(nombre, ubicacion, tipo_carroceria, nombre_marcas, cantmax_modelos, cant_empleados, cant_autos));
                         }
                         break;
                         case 2: {
@@ -185,12 +217,16 @@ public class Lab3_KarimGuifarro_AngelPonce {
                     int p = KaOz.nextInt();
                     switch (p) {
                         case 1: {
-                            String nombre=KaOz.next();
-                            int año_creacion=KaOz.nextInt();
-                            String campo=KaOz.next();
-                            String tech_desarrollada=KaOz.next();
-                            tech.add(new Divisiones_Tech(nombre,campo,tech_desarrollada,año_creacion));
-                            
+                            System.out.println("Ingrese el nombre");
+                            String nombre = KaOz.next();
+                            System.out.println("Ingrese el año en el que se creo");
+                            int año_creacion = KaOz.nextInt();
+                            System.out.println("Ingrese el campo");
+                            String campo = KaOz.next();
+                            System.out.println("Ingrese la tecnologia desarrollada");
+                            String tech_desarrollada = KaOz.next();
+                            tech.add(new Divisiones_Tech(nombre, campo, tech_desarrollada, año_creacion));
+
                         }
                         break;
                         case 2: {
@@ -223,6 +259,7 @@ public class Lab3_KarimGuifarro_AngelPonce {
             }
         }
     }
+
     static void Print(ArrayList corp) {
         System.out.println("Que estado dese ver?");
         String est = "Premium";
